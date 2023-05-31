47b7986efa648d40e81c5c78b9b85a373f13b4ba
sagar-makode <makodepatil1@gmail.com>
2023-06-01 01:18:58 +0530

    change in navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
 <div className="container-xxl position-relative p-0">
             <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
                 <a href="/" className="navbar-brand p-0">
-                    <h1 className="m-0">MakemyImage</h1>
+                    <h1 className="m-0">Make My Image</h1>
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span className="fa fa-bars"></span>
